8bc305de531c4b721b4b8b8ee489ce2bd889caee
Dmitry <graviohz@gmail.com>
2022-09-07 12:06:54 +0300

    update

--- lib/modules/bottom_sheets/components/simple_shade_animation_stack.dart
+++ lib/modules/bottom_sheets/components/simple_shade_animation_stack.dart
@@ -6,24 +6,37 @@ class SShadeAnimationStack extends StatelessWidget {
   const SShadeAnimationStack({
     Key? key,
     required this.child,
-    required this.controller,
+    //required this.controller,
+    required this.showShade,
   }) : super(key: key);
 
   final Widget child;
-  final AnimationController controller;
+  //final AnimationController controller;
+  final bool showShade;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         child,
-        if (controller.value != 0)
-          Container(
-            /// black54 is default system color for shading
-            color: Colors.black54.withOpacity(
-              (controller.value * 100).round() * 0.0054,
+        if (showShade)
+          AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: showShade ? 1.0 : 0.0,
+            child: Container(
+              color: Colors.black54,
             ),
           ),
+        /*
+        if (showShade)
+          Container(
+            /// black54 is default system color for shading
+            //color: Colors.black54.withOpacity(
+            //  (100 * 100).round() * 0.0054,
+            //),
+            color: Colors.black54,
+          ),
+        */
       ],
     );
   }
