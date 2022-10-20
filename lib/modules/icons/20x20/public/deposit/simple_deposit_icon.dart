36065f2a829b70eee4aada56305cf5f11ef7e343
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2022-10-20 16:28:39 +0300

    Fix for icon color

--- lib/modules/icons/20x20/public/deposit/simple_deposit_icon.dart
+++ lib/modules/icons/20x20/public/deposit/simple_deposit_icon.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:simple_kit/core/di.dart';
 import 'package:simple_kit/core/simple_kit.dart';
 import 'package:simple_kit/modules/icons/20x20/light/deposit/simple_light_deposit_icon.dart';
 import 'package:simple_kit/utils/enum.dart';
 
 class SDepositIcon extends StatelessObserverWidget {
-  const SDepositIcon({Key? key}) : super(key: key);
+  const SDepositIcon({
+    Key? key,
+    this.color,
+  }) : super(key: key);
+
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return sKit.currentTheme == STheme.dark
-        ? const SimpleLightDepositIcon()
-        : const SimpleLightDepositIcon();
+        ? SimpleLightDepositIcon(color: color)
+        : SimpleLightDepositIcon(color: color);
   }
 }
