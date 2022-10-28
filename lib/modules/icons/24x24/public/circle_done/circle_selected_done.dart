2f5f23d289052f9a58ac0d3df50eb5980ca79ba4
Dmitry <graviohz@gmail.com>
2022-10-28 13:31:51 +0300

    fix

--- lib/modules/icons/24x24/public/circle_done/circle_selected_done.dart
+++ lib/modules/icons/24x24/public/circle_done/circle_selected_done.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:simple_kit/core/di.dart';
 import 'package:simple_kit/core/simple_kit.dart';
-import 'package:simple_kit/modules/icons/24x24/light/circle_done/circle_done.dart';
+import 'package:simple_kit/modules/icons/24x24/light/circle_done/circle_selected_done.dart';
 import 'package:simple_kit/utils/enum.dart';
 
-class SCircleDone extends StatelessObserverWidget {
-  const SCircleDone({
+class SCircleDoneSelected extends StatelessObserverWidget {
+  const SCircleDoneSelected({
     Key? key,
     this.color,
   }) : super(key: key);
@@ -16,7 +16,7 @@ class SCircleDone extends StatelessObserverWidget {
   @override
   Widget build(BuildContext context) {
     return sKit.currentTheme == STheme.dark
-        ? SimpleLightCircleDoneIcon(color: color)
-        : SimpleLightCircleDoneIcon(color: color);
+        ? SimpleLightCircleDoneSelectedIcon(color: color)
+        : SimpleLightCircleDoneSelectedIcon(color: color);
   }
 }
